7519524c2da797bbaa70a2ce71fa4bc24a824b04
JozefuJotaru <jozefjositarruu@gmail.com>
2025-12-25 19:37:10 +0000

    Add files via upload

---
+++ hiddify2.1.5/lib/core/model/region.dart
@@ -0,0 +1,21 @@
+import 'package:hiddify/core/localization/translations.dart';
+
+enum Region {
+  ir,
+  cn,
+  ru,
+  af,
+  id,
+  tr,
+  other;
+
+  String present(TranslationsEn t) => switch (this) {
+        ir => t.settings.general.regions.ir,
+        cn => t.settings.general.regions.cn,
+        ru => t.settings.general.regions.ru,
+        tr => t.settings.general.regions.tr,
+        af => t.settings.general.regions.af,
+        id => t.settings.general.regions.id,
+        other => t.settings.general.regions.other,
+      };
+}
